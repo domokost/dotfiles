e10cba78e4e8eb4c2eb9eab06a32a9aedc8bd123
Domokos Tar <568522+domokost@users.noreply.github.com>
2022-09-20 15:00:09 +0200

    removed ...

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -32,10 +32,8 @@ elif [ -f /etc/debian_version ]; then
     VER=$(cat /etc/debian_version)
 elif [ -f /etc/SuSe-release ]; then
     # Older SuSE/etc.
-    ...
 elif [ -f /etc/redhat-release ]; then
     # Older Red Hat, CentOS, etc.
-    ...
 else
     # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
     OS=$(uname -s)
